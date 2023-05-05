123dc7f34c35613effe5d285fb6f31320c9ee07b
egiistw88 <egiegay08@gmail.com>
2023-05-05 14:36:22 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam id asperiores hic ipsa dolor repudiandae labore veniam distinctio excepturi quos eligendi nisi rem sunt a voluptate beatae, quae tempora eum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
